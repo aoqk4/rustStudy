42580fffbdd4f73dfe701ca13924453b48182ebc
ljs <qazplm807733@gmail.com>
2023-01-12 19:46:22 +0900

    add test code

--- greprs/src/lib.rs
+++ greprs/src/lib.rs
@@ -2,6 +2,8 @@ use std::error::Error;
 use std::fs::File;
 use std::io::prelude::*;
 
+use test::search;
+
 pub struct Config {
     pub _query: String,
     pub _filename: String,
@@ -26,7 +28,38 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let mut contents = String::new();
     f.read_to_string(&mut contents)?;
 
-    println!("With text:\n{}", contents);
+    for line in search(&config._query, &config._contents) {
+        println!("{line}");
+    }
 
     Ok(())
+}
+
+#[cfg(test)]
+mod test {
+    use std::result;
+
+    use super::*;
+
+    #[test]
+    fn one_result() {
+        let query = "duct";
+        let contents = "/
+Rust:
+safe, fast, productive.
+Pick three.";
+
+        assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+    }
+
+    pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+        let mut results = Vec::new();
+
+        for line in contents.lines() {
+            if line.contains(query) {
+                results.push(line);
+            }
+        }
+        results
+    }
 }
